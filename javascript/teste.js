35d70e685c5855eb1515f72fbe9b960f49e865b5
Felismino Chico <135046240+felisminochico@users.noreply.github.com>
2024-01-20 12:51:52 +0000

    Atualizei os estilos

--- javascript/teste.js
+++ javascript/teste.js
@@ -1,42 +1,52 @@
-// alert('Olá, Mundo!');
+let mudarMenu = false;
 
-// let divMenu = window.document.getElementsByTagName('div')[0];
-// let divIcon = window.document.getElementById('icon');
-// let divHome = window.document.querySelector('div#home');
-// let divTerreno = window.document.getElementById('terreno');
-// let divNoticias = window.document.getElementById('noticias');
-// let divAjuda = window.document.getElementById('ajuda');
-
-let mudar = 0
 
 function Menu(){
-    mudar = 0
-    if(mudar == 0){
-        window.document.getElementsByTagName('nav')[0].style.height = '158px';
-        window.document.getElementById('menu').style.marginLeft = '-100px';
-        window.document.getElementById('menu').style.transitionDuration = '1.2s';
-        window.document.getElementById('menus').style.display = 'flex';
-        window.document.getElementById('menus').style.marginLeft = '0px';
-        window.document.getElementById('menus').style.marginTop = '-20px'; 
-        window.document.getElementById('menus').style.transitionDuration = '1.2s';
-        window.document.getElementById('icon').style.display = 'block'; 
-        window.document.getElementById('icon').style.transitionDuration = '1.2s'; 
+    mudarMenu = !mudarMenu;
 
+    if(mudarMenu == true){
+        window.document.getElementsByTagName('nav')[0].style.height = '158px';
+        window.document.getElementById('menus').style.marginLeft = '0px';
+        window.document.getElementById('menu-telefone').style.marginTop = '-20px';
+        window.document.getElementById('icon').style.textAlign = 'end';
+        window.document.getElementById('menu').style.marginLeft = '-200px'
+        window.document.getElementsByTagName('main')[0].style.filter = 'blur(1px)';
+    }else if(mudarMenu == false){
+        window.document.getElementById('menu').style.marginLeft = '0px';
+        window.document.getElementById('menus').style.marginLeft = '-200px';
+        window.document.getElementById('menus').style.marginTop = '20px';
+        window.document.getElementById('icon').style.textAlign = 'start';
+        window.document.getElementsByTagName('nav')[0].style.height = '20px'
+        window.document.getElementsByTagName('main')[0].style.filter = 'blur(0px)';
     }
 }
 
 function fecharMenu(){
-    mudar = 1
-        if(mudar == 1){
-            window.document.getElementsByTagName('nav')[0].style.height = '20px';
-            window.document.getElementById('menu').style.marginLeft = '0px';
-            window.document.getElementById('menu').style.transitionDuration = '1.2s';
-            window.document.getElementById('menus').style.marginLeft = '-200px';
-            window.document.getElementById('icon').style.display = 'none';
-        }
+    // mudarMenu = !mudarMenu
+
+    if(window.document.body.style.width <= 600 && mudarMenu == true){
+        window.document.getElementById('menu').style.marginLeft = '0px';
+        window.document.getElementById('menus').style.marginLeft = '-400px';
+        window.document.getElementById('menus').style.marginTop = '20px';
+        window.document.getElementById('icon').style.textAlign = 'start';
+        window.document.getElementsByTagName('nav')[0].style.height = '20px'
+        window.document.getElementsByTagName('main')[0].style.filter = 'blur(0px)';
+    }
 }
 
-// function voltarMenu(){
-//     window.document.getElementById('menu').style.display = 'none';
-//     window.document.getElementById('menus').style.display = 'flex';
-// }
+function mudarTamanho(){
+
+    // if(window.document.body.style.width >= 601 && mudarMenu == false){
+    //     window.document.getElementsByTagName('nav')[0].style.height = '20px'
+    //     window.document.getElementById('menus').style.display = 'flex';
+    //     window.document.getElementById('menus').style.columnGap = '22px';
+    //     window.document.getElementById('menus').style.paddingTop = '30px';
+    //     window.document.getElementById('menu').style.display = 'none';
+    //     window.document.getElementById('icon').style.display = 'none';
+    // }
+}
+
+
+
+
+
