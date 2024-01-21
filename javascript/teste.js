7f098701ac0919743a0a6f827c7d351d8de65d1c
Felismino Chico <135046240+felisminochico@users.noreply.github.com>
2024-01-21 10:43:32 +0000

    Criei o cabeçalho do Site

--- javascript/teste.js
+++ javascript/teste.js
@@ -1,49 +1,51 @@
 let mudarMenu = false;
 
-
 function Menu(){
     mudarMenu = !mudarMenu;
 
     if(mudarMenu == true){
-        window.document.getElementsByTagName('nav')[0].style.height = '158px';
-        window.document.getElementById('menus').style.marginLeft = '0px';
-        window.document.getElementById('menu-telefone').style.marginTop = '-20px';
-        window.document.getElementById('icon').style.textAlign = 'end';
-        window.document.getElementById('menu').style.marginLeft = '-200px'
-        window.document.getElementsByTagName('main')[0].style.filter = 'blur(1px)';
+        window.document.getElementById('menu-telefone').style.minHeight = '180px';
+        window.document.getElementById('btn-menu').style.transform = 'translateX(-600px)';
+        window.document.getElementById('menus-telefone').style.transform = 'translate(0px, -35px)';
+        window.document.getElementsByTagName('main')[0].style.filter = 'blur(1.7px)';
     }else if(mudarMenu == false){
-        window.document.getElementById('menu').style.marginLeft = '0px';
-        window.document.getElementById('menus').style.marginLeft = '-200px';
-        window.document.getElementById('menus').style.marginTop = '20px';
-        window.document.getElementById('icon').style.textAlign = 'start';
-        window.document.getElementsByTagName('nav')[0].style.height = '20px'
-        window.document.getElementsByTagName('main')[0].style.filter = 'blur(0px)';
+        window.document.getElementById('menu-telefone').style.minHeight = '58px';
+        window.document.getElementById('btn-menu').style.transform = 'translateX(0px)';
+        window.document.getElementById('menus-telefone').style.transform = 'translate(-700px, 35px)';
+        window.document.getElementsByTagName('main')[0].style.filter = 'none';
     }
 }
 
 function fecharMenu(){
-    // mudarMenu = !mudarMenu
+    mudarMenu = !mudarMenu;
 
-    if(window.document.body.style.width <= 600 && mudarMenu == true){
-        window.document.getElementById('menu').style.marginLeft = '0px';
-        window.document.getElementById('menus').style.marginLeft = '-400px';
-        window.document.getElementById('menus').style.marginTop = '20px';
-        window.document.getElementById('icon').style.textAlign = 'start';
-        window.document.getElementsByTagName('nav')[0].style.height = '20px'
-        window.document.getElementsByTagName('main')[0].style.filter = 'blur(0px)';
+    if(mudarMenu == true){
+        window.document.getElementById('menu-telefone').style.minHeight = '58px';
+        window.document.getElementById('btn-menu').style.transform = 'translateX(0px)';
+        window.document.getElementById('menus-telefone').style.transform = 'translate(-700px, 35px)';
+        // window.document.getElementsByTagName('main')[0].style.filter = 'none';
+    }else if(mudarMenu == false){
+        window.document.getElementById('menu-telefone').style.minHeight = '58px';
+        window.document.getElementById('btn-menu').style.transform = 'translateX(0px)';
+        window.document.getElementById('menus-telefone').style.transform = 'translate(-700px, 35px)';
+        window.document.getElementsByTagName('main')[0].style.filter = 'none';
     }
 }
 
 function mudarTamanho(){
+    mudarMenu = !mudarMenu;
 
-    // if(window.document.body.style.width >= 601 && mudarMenu == false){
-    //     window.document.getElementsByTagName('nav')[0].style.height = '20px'
-    //     window.document.getElementById('menus').style.display = 'flex';
-    //     window.document.getElementById('menus').style.columnGap = '22px';
-    //     window.document.getElementById('menus').style.paddingTop = '30px';
-    //     window.document.getElementById('menu').style.display = 'none';
-    //     window.document.getElementById('icon').style.display = 'none';
-    // }
+    if(mudarMenu == true){
+        window.document.getElementById('menu-telefone').style.minHeight = '58px';
+        window.document.getElementById('btn-menu').style.transform = 'translateX(0px)';
+        window.document.getElementById('menus-telefone').style.transform = 'translate(-700px, 35px)';
+        window.document.getElementsByTagName('main')[0].style.filter = 'none'; 
+    }else if(mudarMenu == false){
+        window.document.getElementById('menu-telefone').style.minHeight = '58px';
+        window.document.getElementById('btn-menu').style.transform = 'translateX(0px)';
+        window.document.getElementById('menus-telefone').style.transform = 'translate(-700px, 35px)';
+        window.document.getElementsByTagName('main')[0].style.filter = 'none';
+    }
 }
 
 
