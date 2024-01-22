03637618d3f0273ea85dcd9720986a77f7e8a3e8
Felismino Chico <135046240+felisminochico@users.noreply.github.com>
2024-01-22 16:03:59 +0000

    Eliminiei as configurações antigas e configurei o Site com os novos estilos.

--- javascript/teste.js
+++ javascript/teste.js
@@ -1,4 +1,16 @@
+function Menu(){
+    window.document.getElementById('links-cabecalho').style.left = '0px';
+    window.document.getElementById('imagem-cabecalho').style.filter = 'blur(1px)';
+    window.document.getElementById('btn-menu').style.filter = 'blur(1px)';
+    window.document.getElementsByTagName('main')[0].style.filter = 'blur(1.5px)';
+}
 
+function fecharMenu(){
+    window.document.getElementById('links-cabecalho').style.left = '-300px';
+    window.document.getElementById('imagem-cabecalho').style.filter = 'none';
+    window.document.getElementById('btn-menu').style.filter = 'none';
+    window.document.getElementsByTagName('main')[0].style.filter = 'none';
+}
 
 
 
