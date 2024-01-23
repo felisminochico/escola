8a913c77426bc9751d9c66fd83b0bfe70ab03241
Felismino Chico <135046240+felisminochico@users.noreply.github.com>
2024-01-23 08:47:27 +0000

    Primeiro passo do Site!

--- javascript/teste.js
+++ javascript/teste.js
@@ -3,6 +3,7 @@ function Menu(){
     window.document.getElementById('imagem-cabecalho').style.filter = 'blur(1px)';
     window.document.getElementById('btn-menu').style.filter = 'blur(1px)';
     window.document.getElementsByTagName('main')[0].style.filter = 'blur(1.5px)';
+    window.document.getElementsByTagName('footer')[0].style.filter = 'blur(1.5px)';
 }
 
 function fecharMenu(){
@@ -10,6 +11,7 @@ function fecharMenu(){
     window.document.getElementById('imagem-cabecalho').style.filter = 'none';
     window.document.getElementById('btn-menu').style.filter = 'none';
     window.document.getElementsByTagName('main')[0].style.filter = 'none';
+    window.document.getElementsByTagName('footer')[0].style.filter = 'none';
 }
 
 
